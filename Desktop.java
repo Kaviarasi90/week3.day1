f140f7646366e654de3dfdd8b166329d1db2fffc
Kaviarasi90 <101966808+Kaviarasi90@users.noreply.github.com>
2022-04-30 14:18:20 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,16 @@
+package org.system;
+
+public class Desktop extends Computer{
+	public void desktopSize() {
+		System.out.println("this desktop size is bigger than windows 9");
+		
+	}
+	public static void main(String[] args) {
+		Desktop desktop=new Desktop();
+		desktop.desktopSize();
+		desktop.computerModel();
+		
+		
+	}
+
+}
