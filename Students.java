cfb7afd1575619fbb9f5105d9a8b9e854e9817b4
Kaviarasi90 <101966808+Kaviarasi90@users.noreply.github.com>
2022-04-30 15:15:08 +0530

    Add files via upload

--- Students.java
+++ Students.java
@@ -3,7 +3,8 @@ package week3.day1.assignment;
 public class Students {
 	public int getStudInfo(int id) {
 		System.out.println(id);
-		return id;	
+		return id;
+			
 	}
 	public void getStudInfo(String name,int id) {
 		System.out.println(name+id);
@@ -17,8 +18,8 @@ public class Students {
 	public static void main(String[] args) {
 		Students std=new Students();
 		std.getStudInfo(87);
-		std.getStudInfo("jk", 9609876);
-		std.getStudInfo("ok", 22, "kavijk90@gmail.com");
+		std.getStudInfo("jk  ", 9609876);
+		std.getStudInfo("ok  ", 22, "  kavijk90@gmail.com");
 	
 	}
 
